4f0d1c9dfbc0480a2fec6dca367b6b5249bfee0e
simon chalmers <simon.chalmers@students.mq.edu.au>
2019-11-30 18:41:09 +1100

    10929 - You can say 11 solution

--- Main.java
+++ Main.java
@@ -10,27 +10,17 @@
         File file = new File("input.txt");
         Scanner sc = new Scanner(file);
         while(sc.hasNextLine()){
-            String[] nums = sc.nextLine().split(" ");
-            if(nums[0].equals("0") && nums[1].equals("0")){
-                break;
+            String line = sc.nextLine();
+            line = line.replaceAll("\\s+","");
+            if(line.equals("0")) break;
+            BigInteger num = new BigInteger(line);
+            num.mod(BigInteger.valueOf(11));
+            if(num.mod(BigInteger.valueOf(11))==BigInteger.valueOf(0)){
+                print(line+" is a multiple of 11.\n");
             }
-            long one = Long.parseLong(nums[0]);
-            long two = Long.parseLong(nums[1]);
-            long totalCarry = 0;
-            long carry = 0;
-            while(one!=0 || two!=0){
-                long a = one%10;
-                long b = two%10;
-                long result = a+b+carry;
-                if(result > 9) carry = 1;
-                if(result <= 9) carry = 0;
-                totalCarry += carry;
-                two = two/10;
-                one = one/10;
+            else {
+                print(line+" is not a multiple of 11.\n");
             }
-            if(totalCarry==0) print("No carry operation.\n");
-            if(totalCarry==1) print("1 carry operation.\n");
-            if(totalCarry > 1) print(totalCarry+" carry operations.\n");
         }
     }
 
